2a1d7a808e49c7dde042be1334525fc9992e8f55
OpsConverge <OpsConverge@gmail.com>
2025-11-05 15:03:48 -0600

    update

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './artifacts',
+      outputName: 'jest-junit.xml',
+    }]
+  ],
+};
+
